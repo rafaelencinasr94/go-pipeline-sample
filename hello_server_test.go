2d00da2aecf5ac07cf28ead066f79a332a36dc04
rafaelencinasr94 <rafaelencinasr@gmail.com>
2025-02-26 12:22:13 -0700

    Remove intentional test fail

--- hello_server_test.go
+++ hello_server_test.go
@@ -16,15 +16,6 @@ func TestGreetingSpecificDemo(t *testing.T) {
 	}
 }
 
-func TestShowFailure(t *testing.T) {
-	greeting := CreateGreeting("Demo1")
-	if greeting != "Hello, Demo\n" {
-		t.Errorf("Intentional failure. got: %s, want: %s.", greeting, "Hello, Demo\n")
-	}
-}
-
-
-
 func TestGreetingDefault(t *testing.T) {
 	greeting := CreateGreeting("")
 	if greeting != "Hello, Guest\n" {
